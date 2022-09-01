abfa906b6708583d488d99ec044526cba16a7368
mohamed mokhtar <moamedmotar755@gmail.com>
2022-09-01 19:05:42 +0200

    first for now

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -1,6 +1,12 @@
 <?php
 
 namespace App\Http\Controllers;
+use App\Http\Controllers\admin\CategoryController;
+use App\Http\Controllers\admin\TagController;
+use App\Http\Controllers\admin\ProductController;
+use App\Models\Category; 
+use App\Models\Tag; 
+use App\Models\Product; 
 
 use Illuminate\Http\Request;
 
@@ -8,6 +14,9 @@ class HomeController extends Controller
 {
     public function index()
     {
-        return view('website.index');
+        $categories=Category::all();
+        $tags=Tag::all();
+        $products = Product::all();
+        return view('website.index',["categories"=>$categories, "tags"=>$tags,"products"=>$products]);
     }
 }
