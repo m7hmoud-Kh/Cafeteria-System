3da44c4fd7dff47b9cec382e39631fa897ade825
Mohamed Mohei <mohamedmohei1920@gmail.com>
2022-09-03 14:31:42 +0200

    ff

--- resources/views/website/index.blade.php
+++ resources/views/website/index.blade.php
@@ -30,32 +30,6 @@
                         </ul>
                     @endforeach
                 </div>
-                <div class="col-lg-9 order-1 order-lg-2 mb-5 mb-lg-0">
-                    <div class="row">
-                        <!-- PRODUCT-->
-                        @foreach ($products as $p)
-                            <div class="col-lg-4 col-sm-6">
-                                <div class="product text-center">
-                                    <div class="mb-3 position-relative">
-                                        <div class="badge text-white badge-"></div>
-                                        <a class="d-block" href="#">
-                                            <img class="img-fluid" src="{{ asset('product_image/' . $p->image) }}"
-                                                alt="{{ $p->name }}">
-                                        </a>
-                                        <div class="product-overlay">
-                                            <ul class="mb-0 list-inline">
-                                                <li class="list-inline-item m-0 p-0"><a class="btn btn-sm btn-dark"
-                                                        href="#">Add to cart</a></li>
-                                            </ul>
-                                        </div>
-                                    </div>
-                                    <h6> <a class="reset-anchor" href="#">{{ $p->name }}</a></h6>
-                                    <p class="small text-muted">{{ $p->price }} EGP</p>
-                                </div>
-                            </div>
-                        @endforeach
-                    </div>
-                </div>
                 <livewire:website.product-shop-component :products="$products">
             </div>
         </div>
