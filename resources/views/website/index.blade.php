ab9340f7ce2662541834c7633b4b8eb7a20ca82c
mohamed mokhtar <moamedmotar755@gmail.com>
2022-09-03 14:12:33 +0200

    here

--- resources/views/website/index.blade.php
+++ resources/views/website/index.blade.php
@@ -10,31 +10,7 @@
 
 @section('content')
 
-<div class="row">
-    <!-- SHOP SIDEBAR-->
-    <div class="col-lg-3 order-2 order-lg-1">
-     <!-- <h5 class="text-uppercase mb-4">Categories</h5>-->
-      <div class="py-2 px-4 bg-dark text-white mb-3"><strong class="small text-uppercase font-weight-bold">Categories</strong></div>
-         
-      @foreach ($categories as $category)
-      
-      <ul class="list-unstyled small text-muted pl-lg-4 font-weight-normal">
-        <li class="mb-2"><a class="reset-anchor" href="#"> {{ $category->name }}</a></li>
-      </ul>
 
-     @endforeach
-
-         <div class="py-2 px-4 bg-dark text-white mb-3"><strong class="small text-uppercase font-weight-bold"> tags</strong></div>
-     
-         @foreach ($tags as $tag)
-        
-           
-            <ul class="list-unstyled small text-muted pl-lg-4 font-weight-normal">
-                <li class="mb-2"><a class="reset-anchor" href="#">{{$tag->name}}</a></li>
-            </ul>
-         @endforeach
-
-    </div>
 
     <section class="py-5">
         <div class="container p-0">
