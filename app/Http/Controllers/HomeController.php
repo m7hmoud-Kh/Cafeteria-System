9595e1f02ccc9a31f913e7c297a8ac9c7a612c80
Mohamed Mohei <mohamedmohei1920@gmail.com>
2022-09-03 13:07:39 +0200

    pull 3/9

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -1,6 +1,7 @@
 <?php
 namespace App\Http\Controllers;
 use App\Models\Tag;
+use App\Models\Product;
 
 use App\Models\Category;
 
@@ -8,15 +9,11 @@ class HomeController extends Controller
 {
     public function index()
     {
-
         $categories = Category::WhereHas('product' ,function($query) {
             $query->where('status', true);
         })->select('id','name')->get();
         $tags=Tag::all();
         $products = Product::whereStatus(true)->where('quantity' , '>=' , '1')->select('id','name','image','price')->get();
         return view('website.index',["categories"=>$categories, "tags"=>$tags,"products"=>$products]);
-        return view('website.index', [
-            'users' => DB::table('users')->paginate(15)
-        ]);
     }
 }
