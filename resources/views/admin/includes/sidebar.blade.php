34748373134b475b7130c7861a1a3bec8986f49a
Mohamed Mohei <mohamedmohei1920@gmail.com>
2022-09-04 23:33:00 +0200

    Fix Sidebar

--- resources/views/admin/includes/sidebar.blade.php
+++ resources/views/admin/includes/sidebar.blade.php
@@ -1,18 +1,6 @@
 <div class="side-menu-fixed">
     <div class="scrollbar side-menu-bg">
         <ul class="nav navbar-nav side-menu" id="sidebarnav">
-            <a href="javascript:void(0);" data-toggle="collapse" data-target="#calendar-menu">
-                <div class="pull-left"><i class="ti-calendar"></i><span class="right-nav-text">Orders</span>
-                </div>
-                <div class="pull-right"><i class="ti-plus"></i></div>
-                <div class="clearfix"></div>
-            </a>
-            <ul id="calendar-menu" class="collapse" data-parent="#sidebarnav">
-                <li> <a href="{{ route('orders') }}">All Orders</a> </li>
-                <li> <a href="{{ route('processingOrders') }}">processing</a> </li>
-                <li> <a href="{{ route('outOfDeliveryOrders') }}">Out of Delivery</a> </li>
-                <li> <a href="{{ route('doneOrders') }}">Done</a> </li>
-            </ul>
             <li>
                 <a href="{{ route('admin.index') }}"><i class="fa fa-user-plus"></i><span
                         class="right-nav-text">Admin</span> </a>
@@ -22,7 +10,8 @@
                 </a>
             </li>
             <li>
-                <a href="{{ route('tag.index') }}"><i class="fa fa-tag"></i><span class="right-nav-text">Tag</span> </a>
+                <a href="{{ route('tag.index') }}"><i class="fa fa-tag"></i><span class="right-nav-text">Tag</span>
+                </a>
             </li>
             <li>
                 <a href="{{ route('category.index') }}"><i class="fa fa-user-o"></i><span
@@ -37,6 +26,19 @@
                         <a href="{{ route('make-order') }}"><i class="fa fa-cart-plus"></i><span
                                 class="right-nav-text">Cart
                             </span></a>
+            </li>
+            <li>
+                <a href="javascript:void(0);" data-toggle="collapse" data-target="#order-menu">
+                    <div class="pull-left"><i class="ti-calendar"></i><span class="right-nav-text">Orders</span></div>
+                    <div class="pull-right"><i class="ti-plus"></i></div>
+                    <div class="clearfix"></div>
+                </a>
+                <ul id="order-menu" class="collapse" data-parent="#sidebarnav">
+                    <li> <a href="{{ route('orders') }}">All Orders</a> </li>
+                    <li> <a href="{{ route('processingOrders') }}">processing</a> </li>
+                    <li> <a href="{{ route('outOfDeliveryOrders') }}">Out of Delivery</a> </li>
+                    <li> <a href="{{ route('doneOrders') }}">Done</a> </li>
+                </ul>
             </li>
         </ul>
     </div>
